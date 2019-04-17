e729af1d359eda3663fc939ae97e1c702a57227e
Brendan <okrek6@gmail.com>
2019-04-17 16:34:07 -0500

    Added Scrolling from Retrospective Example

---
+++ Teleprompter/Orientation.swift
@@ -0,0 +1,14 @@
+    //
+//  Orientation.swift
+//  Teleprompter
+//
+//  Created by Krekeler, Brendan F. (MU-Student) on 4/17/19.
+//  Copyright © 2019 Krekeler, Brendan F. (MU-Student). All rights reserved.
+//
+
+import UIKit
+
+struct Orientation {
+    let x: CGFloat
+    let y: CGFloat
+}
