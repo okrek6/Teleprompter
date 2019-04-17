d1ff5a0c8f4a5d7408976542de7b4c43c127656a
Brendan <okrek6@gmail.com>
2019-04-16 23:11:26 -0500

    added interface

---
+++ Teleprompter/TeleprompterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TeleprompterViewController.swift
+//  Teleprompter
+//
+//  Created by Krekeler, Brendan F. (MU-Student) on 4/16/19.
+//  Copyright © 2019 Krekeler, Brendan F. (MU-Student). All rights reserved.
+//
+
+import UIKit
+
+class TeleprompterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
